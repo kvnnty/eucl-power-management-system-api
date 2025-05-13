7fc85fff2a1ab214cc8fe759de58017557cf77a9
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2025-05-13 11:17:35 +0200

    chore: ground zero

---
+++ src/main/java/com/kvn/eucl/powermetersystem/PowermetersystemApplication.java
@@ -0,0 +1,13 @@
+package com.kvn.eucl.powermetersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowermetersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowermetersystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kvn/eucl/powermetersystem/PowermetersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvn.eucl.powermetersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowermetersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
